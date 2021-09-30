d9ad955d6ca6b30d598853645a070f41728b1e1e
nProgrammer <wagner.norbert277@gmail.com>
2021-09-30 21:45:49 +0200

    updated function that is loading authorized packages list

--- config/load-authorize-packages.go
+++ config/load-authorize-packages.go
@@ -4,11 +4,20 @@ import (
 	"encoding/json"
 	"ghpm/models"
 	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
 func LoadAuthorizedPackagesList() models.LicensedPackages {
-	file, _ := ioutil.ReadFile("./authos.json")
+	cmd := exec.Command("wget", "https://github.com/nProgrammer/github-package-manager/blob/main/authorized-packages.json")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	file, _ := ioutil.ReadFile("./authorized-packages.json")
 	data := models.LicensedPackages{}
 	_ = json.Unmarshal([]byte(file), &data)
+	cmd = exec.Command("rm", "./authorized-packages.json")
+	cmd.Run()
+
 	return data
 }
